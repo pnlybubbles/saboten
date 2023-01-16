3e49e9c0efd3d0341ec39545e68f92dbac8dd2f4
pnly <pnlybubbles@gmail.com>
2023-01-16 14:48:39 +0900

    React.StrictModeを有効化

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,11 @@
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import { StrictMode } from 'react'
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root = createRoot(document.getElementById('app')!)
-root.render(<App />)
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
