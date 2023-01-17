20d5fb0cfcbf6c13052e975f53a4cdcebb90b570
pnly <pnlybubbles@gmail.com>
2023-01-17 22:17:59 +0900

    localstorageのキーが変更されたらstateを即座にnullにする

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,14 +1,28 @@
 import { useEffect, useState } from 'react'
 import type { LocalStorageDescriptor } from '@/utils/localstorage'
 
-export const useLocalStorage = <T>(descriptor: LocalStorageDescriptor<T>) => {
+export const useLocalStorage = <T>(descriptor: LocalStorageDescriptor<T> | undefined) => {
   const [state, setState] = useState<T | null>(null)
+  const [ready, setReady] = useState<null | string>()
 
   useEffect(() => {
+    if (descriptor === undefined) {
+      return
+    }
+
     setState(descriptor.get())
+    setReady(descriptor.key)
 
     return descriptor.observe(() => setState(descriptor.get()))
   }, [descriptor])
 
-  return [state, descriptor.set] as const
+  if (descriptor === undefined || descriptor.key !== ready) {
+    return [null, noop, false] as const
+  }
+
+  return [state, descriptor.set, true] as const
+}
+
+const noop = () => {
+  console.warn('Nothing updated. Storage descriptor is not prepared.')
 }
