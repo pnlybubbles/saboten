654c7e2066c8457052e49c4ce1033508d96f4a10
pnly <pnlybubbles@gmail.com>
2023-01-19 22:45:00 +0900

    ボタンのtouch対応

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,24 +1,28 @@
+import isSP from '@/utils/basic/isSP'
 import clsx from 'clsx'
 
 interface OwnProps {
   primary?: boolean
+  onClick?: React.EventHandler<React.SyntheticEvent<unknown>>
 }
 
-type Props = React.ComponentPropsWithoutRef<'button'> & OwnProps
+type Props = Omit<React.ComponentPropsWithoutRef<'button'>, 'onClick'> & OwnProps
 
-export default function Button({ primary = false, className, ...props }: Props) {
+export default function Button({ primary = false, className, onClick, disabled, ...props }: Props) {
   const backgroundColor = toBackgroundColor(primary)
   const foregroundColor = toForegroundColor(primary)
 
   return (
     <button
-      {...props}
       className={clsx(
         className,
         'h-12 px-5 rounded-full font-bold w-full active:scale-95 transition text-base disabled:opacity-30 select-none',
         backgroundColor,
         foregroundColor,
       )}
+      disabled={disabled}
+      {...(disabled ? {} : isSP ? { onTouchEnd: onClick } : { onClick })}
+      {...props}
     ></button>
   )
 }
---
+++ src/utils/basic/isSP.ts
@@ -0,0 +1,1 @@
+export default /(iP(hone|(o|a)d))|Android/.test(navigator.userAgent)
