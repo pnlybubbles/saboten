b76c5078ca93aba4809c8030f388b86cc4d247fd
pnly <pnlybubbles@gmail.com>
2023-01-22 00:00:00 +0900

    アイコンの色がランダムで決まるようにした

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,4 +1,6 @@
+import stringToHash from '@/utils/basic/stringToHash'
 import clsx from 'clsx'
+import { useMemo } from 'react'
 
 interface Props {
   name: string | null
@@ -7,11 +9,14 @@ interface Props {
 }
 
 export default function Avatar({ name, className, mini }: Props) {
+  const hashed = useMemo(() => (name ? stringToHash(name) : null), [name])
+
   return (
     <div
       className={clsx(
-        'grid h-12 w-12 items-center justify-items-center rounded-full bg-primary text-lg font-bold text-white',
+        'grid h-12 w-12 items-center justify-items-center rounded-full text-lg font-bold text-white',
         mini && 'h-10 w-10',
+        hashed !== null ? COLORS[hashed % COLORS.length] : 'bg-zinc-400',
         className,
       )}
     >
@@ -19,3 +24,24 @@ export default function Avatar({ name, className, mini }: Props) {
     </div>
   )
 }
+
+const COLORS = [
+  'bg-primary',
+  'bg-red-400',
+  'bg-orange-400',
+  'bg-amber-400',
+  'bg-yellow-400',
+  'bg-lime-400',
+  'bg-green-400',
+  'bg-emerald-400',
+  'bg-teal-400',
+  'bg-cyan-400',
+  'bg-sky-400',
+  'bg-blue-400',
+  'bg-indigo-400',
+  'bg-violet-400',
+  'bg-purple-400',
+  'bg-fuchsia-400',
+  'bg-pink-400',
+  'bg-rose-400',
+]
