53bb6228cce56e65f3d4bfbc1c374a34154ac3cc
pnly <pnlybubbles@gmail.com>
2023-01-22 17:42:59 +0900

    工事中を明記した

--- src/App.tsx
+++ src/App.tsx
@@ -80,9 +80,19 @@ function Landing() {
       <Create />
     )
   ) : stage === 'restore' ? (
-    <div>
-      <div>以前のスクショした合言葉を入力してください</div>
-      <TextField onChange={() => void 0} />
+    <div className="grid gap-4">
+      <div className="grid grid-flow-col items-center justify-start gap-1 rounded-lg bg-red-100 p-4 text-xs text-red-900">
+        <Icon name="construction"></Icon>
+        <div>
+          {`Now under construction... `}
+          <span className="underline" onClick={() => setStage('start')}>
+            {'< Back'}
+          </span>
+        </div>
+      </div>
+      <div className="font-bold">合言葉を入力してユーザー情報を復元します</div>
+      <TextField label="合言葉" name="secret" onChange={() => void 0} />
+      <Button disabled>復元する</Button>
     </div>
   ) : (
     unreachable(stage)
