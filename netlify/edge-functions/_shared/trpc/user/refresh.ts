8ba11537a5e73d4fd340bc18773647d39e3879b3
pnly <pnlybubbles@gmail.com>
2023-01-23 20:27:53 +0900

    ユーザーが見つからない場合はcookieを削除する

--- netlify/edge-functions/_shared/trpc/user/refresh.ts
+++ netlify/edge-functions/_shared/trpc/user/refresh.ts
@@ -1,11 +1,17 @@
 import prisma from '../../prisma.ts'
 import { publicProcedure } from '../server.ts'
 
-export default publicProcedure.mutation(async ({ ctx: { userId, setCookie } }) => {
+export default publicProcedure.mutation(async ({ ctx: { userId, setCookie, removeCookie } }) => {
   if (!userId) {
     return null
   }
-  setCookie('id', userId, { maxAge: 60 * 60 * 24 * 365 * 2 })
   const user = await prisma.user.findUnique({ where: { id: userId } })
+  if (user) {
+    // ユーザーが見つかったらcookieを延長
+    setCookie('id', userId, { maxAge: 60 * 60 * 24 * 365 * 2 })
+  } else {
+    // ユーザーが見つからないのであればcookieを削除する
+    removeCookie('id')
+  }
   return user
 })
