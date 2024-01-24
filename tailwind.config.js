318213d0e44e626f84ddfc2fd063f5257a487dd9
pnly <pnlybubbles@gmail.com>
2024-01-24 17:35:08 +0900

    appディレクトリに移動

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js,ts,jsx,tsx}'],
+  content: ['./src/app/**/*.{html,js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
