ab3e147b438687dc84c1db265d92d903a638f868
pnly <pnlybubbles@gmail.com>
2024-02-11 04:23:44 +0900

    プロジェクトルートにビルド結果を書き出す

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   root: 'src/app',
+  build: {
+    outDir: '../../dist',
+  },
   plugins: [
     react(),
     tsconfigPaths({
