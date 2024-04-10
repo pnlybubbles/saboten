b852a2e5661541c6bab5655c5ca6628509b766ad
pnly <pnlybubbles@gmail.com>
2024-04-10 19:25:55 +0900

    StrictModeを有効にしているとネストしたドロワーがiOS Safariでバグる問題を修正

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import App from './App'
-import { StrictMode } from 'react'
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root = createRoot(document.getElementById('app')!)
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+root.render(<App />)
