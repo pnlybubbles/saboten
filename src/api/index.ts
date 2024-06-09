171133cb73b9d95013876520e6cc062e5e25224d
pnly <pnlybubbles@gmail.com>
2024-06-10 00:44:16 +0900

    APIのログを出す

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,6 @@
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
+import { logger } from 'hono/logger'
 import type { Env } from './type'
 import user from './user'
 import room from './room'
@@ -7,6 +8,7 @@ import event from './event'
 
 const api = new Hono<Env>()
   .basePath('/api')
+  .use(logger())
   .use('*', cors())
   .get('/', (c) => {
     return c.json({ hello: true })
