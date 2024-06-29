db94ef52d665443982065472f668b976e98a150e
pnly <pnlybubbles@gmail.com>
2024-06-30 07:28:59 +0900

    静的アセットをimportで書く

--- src/app/components/Logo.tsx
+++ src/app/components/Logo.tsx
@@ -1,10 +1,11 @@
 import clsx from 'clsx'
+import logo from '../icon/logo.png'
 
 export default function Logo({ big }: { big?: boolean }) {
   const size = big ? 48 : 36
   return (
     <div className={clsx('grid justify-items-center', big ? 'gap-2 py-6' : 'gap-1 py-2')}>
-      <img src="../icon/logo.png" alt="" width={size} height={size} />
+      <img src={logo} alt="" width={size} height={size} />
       <div className={clsx('font-bold', big ? 'text-lg' : 'text-sm')}>SABOTEN</div>
     </div>
   )
