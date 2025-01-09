7ca8d835cd08179c43b11d7ecd8ec07e5215d1bf
pnly <pnlybubbles@gmail.com>
2025-01-09 17:10:15 +0900

    画像の上の方がいらないのでクロップ

--- src/app/scene/Landing/Screenshots.tsx
+++ src/app/scene/Landing/Screenshots.tsx
@@ -1,7 +1,7 @@
 export default function ScreenShots() {
   return (
     <div className="-mx-8 -my-12 grid grid-flow-col gap-4 overflow-x-scroll px-8 py-12">
-      <div className="grid w-[calc(100vw-4rem)] gap-3">
+      <div className="grid gap-3">
         <div className="px-2 text-xs text-zinc-400">
           <span className="font-bold">支払いを記録</span>
           <br />
@@ -10,18 +10,22 @@ export default function ScreenShots() {
         <img
           src="/ss/1.webp"
           alt="支払いの記録が一覧できる画面"
-          className="shadow-float-high overflow-hidden rounded-xl"
+          className="shadow-float-high h-[137vw] w-[70vw] max-w-none overflow-hidden rounded-xl object-cover [object-position:0_81%]"
         />
       </div>
-      <div className="grid w-[calc(100vw-4rem)] gap-3">
+      <div className="grid gap-3">
         <div className="px-2 text-xs text-zinc-400">
           <span className="font-bold">イベントを追加</span>
           <br />
           <span>誰が支払って、誰と割り勘したか、を記録できます。</span>
         </div>
-        <img src="/ss/2.webp" alt="イベントを追加する画面" className="shadow-float-high overflow-hidden rounded-xl" />
+        <img
+          src="/ss/2.webp"
+          alt="イベントを追加する画面"
+          className="shadow-float-high h-[137vw] w-[70vw] max-w-none overflow-hidden rounded-xl object-cover [object-position:0_81%]"
+        />
       </div>
-      <div className="grid w-[calc(100vw-4rem)] gap-3">
+      <div className="grid gap-3">
         <div className="px-2 text-xs text-zinc-400">
           <span className="font-bold">URL共有</span>
           <br />
@@ -30,10 +34,10 @@ export default function ScreenShots() {
         <img
           src="/ss/3.webp"
           alt="メンバーを追加したり管理する画面"
-          className="shadow-float-high overflow-hidden rounded-xl"
+          className="shadow-float-high h-[137vw] w-[70vw] max-w-none overflow-hidden rounded-xl object-cover [object-position:0_81%]"
         />
       </div>
-      <div className="grid w-[calc(100vw-4rem)] gap-3">
+      <div className="grid gap-3">
         <div className="px-2 text-xs text-zinc-400">
           <span className="font-bold">最適な精算</span>
           <br />
@@ -42,10 +46,10 @@ export default function ScreenShots() {
         <img
           src="/ss/4.webp"
           alt="最適な生産方法を提示している画面"
-          className="shadow-float-high overflow-hidden rounded-xl"
+          className="shadow-float-high h-[137vw] w-[70vw] max-w-none overflow-hidden rounded-xl object-cover [object-position:0_81%]"
         />
       </div>
-      <div className="grid w-[calc(100vw-4rem)] gap-3">
+      <div className="grid gap-3">
         <div className="px-2 text-xs text-zinc-400">
           <span className="font-bold">記録の管理</span>
           <br />
@@ -54,10 +58,10 @@ export default function ScreenShots() {
         <img
           src="/ss/5.webp"
           alt="いままでの記録を参照する画面"
-          className="shadow-float-high overflow-hidden rounded-xl"
+          className="shadow-float-high h-[137vw] w-[70vw] max-w-none overflow-hidden rounded-xl object-cover [object-position:0_81%]"
         />
       </div>
-      <div className="grid w-[calc(100vw-4rem)] gap-3">
+      <div className="grid gap-3">
         <div className="px-2 text-xs text-zinc-400">
           <span className="font-bold">外国の通貨</span>
           <br />
@@ -66,7 +70,7 @@ export default function ScreenShots() {
         <img
           src="/ss/6.webp"
           alt="複数の通貨が混ざっている支払い記録の画面"
-          className="shadow-float-high overflow-hidden rounded-xl"
+          className="shadow-float-high h-[137vw] w-[70vw] max-w-none overflow-hidden rounded-xl object-cover [object-position:0_81%]"
         />
       </div>
     </div>
